a5e176faa139ff3ab5b0ee664c15bdfa77d5a876
Niall Turbitt <niall.turbitt@databricks.com>
2022-04-01 10:18:24 +0100

    spark add

--- telco_churn/data_ingest.py
+++ telco_churn/data_ingest.py
@@ -1,5 +1,7 @@
 from pyspark.sql.dataframe import DataFrame as SparkDataFrame
 
+from telco_churn.utils.get_spark import spark
+
 
 def spark_load_table(table: str) -> SparkDataFrame:
     return spark.table(table)
