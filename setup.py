3e5396e2c708b9d0cc5cc89f2b7a71423424edf8
Niall Turbitt <niall.turbitt@databricks.com>
2022-04-01 09:43:21 +0100

    reqs updated, setup.py use reqs

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 from telco_churn import __version__
 
+with open("requirements.txt") as f:
+    reqs = f.read()
+
 setup(
     name="telco_churn",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
+    install_requires=reqs,
     version=__version__,
     description="",
     author=""
