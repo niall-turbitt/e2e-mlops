6986e58bf23b04b30f8ce304e3121c095ac07230
Niall Turbitt <niall.turbitt@databricks.com>
2022-04-12 10:28:18 +0100

    model evaluation utils updated

--- telco_churn/utils/evaluation_utils.py
+++ telco_churn/utils/evaluation_utils.py
@@ -4,7 +4,7 @@ from sklearn.metrics import roc_auc_score
 
 class ModelEvaluation:
 
-    def __init__(self, metric_prefix: str = "test_"):
+    def __init__(self, metric_prefix: str = ""):
         self.metric_prefix = metric_prefix
 
     @staticmethod
@@ -16,5 +16,5 @@ class ModelEvaluation:
 
     def evaluate(self, y_true: pd.Series, y_score: pd.Series) -> dict:
         return {
-            f"{self.metric_prefix}roc_auc": self._roc_auc_score(y_true, y_score),
+            f"{self.metric_prefix}roc_auc_score": self._roc_auc_score(y_true, y_score),
         }
