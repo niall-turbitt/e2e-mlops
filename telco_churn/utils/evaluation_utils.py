b5ab06f3b23838b6d10b763af12cdf36a2a5acca
Niall Turbitt <niall.turbitt@databricks.com>
2022-04-12 11:29:13 +0100

    model evaluation docstring added

--- telco_churn/utils/evaluation_utils.py
+++ telco_churn/utils/evaluation_utils.py
@@ -10,12 +10,44 @@ class ModelEvaluation:
 
     @staticmethod
     def _roc_auc_score(y_true: pd.Series, y_score: pd.Series):
+        """
+        Compute ROC AUC score using sklearn. Computed in same way as MLflow utils
+        https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_auc_score.html
+        By default, for roc_auc_score, we pick `average` to be `weighted`, `multi_class` to be `ovo`,
+        to make the output more insensitive to dataset imbalance.
+
+        Parameters
+        ----------
+        y_true : array-like of shape (n_samples,) or (n_samples, n_classes)
+            True labels or binary label indicators
+
+        y_score : array-like of shape (n_samples,) or (n_samples, n_classes)
+            Target scores.
+
+        Returns
+        -------
+        auc : float
+        """
         return roc_auc_score(y_true=y_true,
                              y_score=y_score,
                              average="weighted",
                              multi_class="ovo")
 
     def evaluate(self, y_true: pd.Series, y_score: pd.Series) -> Dict:
+        """
+
+        Parameters
+        ----------
+        y_true : array-like of shape (n_samples,) or (n_samples, n_classes)
+            True labels or binary label indicators
+
+        y_score : array-like of shape (n_samples,) or (n_samples, n_classes)
+            Target scores.
+
+        Returns
+        -------
+        Dictionary of (metric name, computed value)
+        """
         return {
             f"{self.metric_prefix}roc_auc_score": self._roc_auc_score(y_true, y_score),
         }
