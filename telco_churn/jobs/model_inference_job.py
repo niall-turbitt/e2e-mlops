2253d661e79a6fff98b30f0f617fe7f32407eae4
Niall Turbitt <niall.turbitt@databricks.com>
2022-06-17 14:39:20 +0100

    model inference batch job updated

--- telco_churn/jobs/model_inference_job.py
+++ telco_churn/jobs/model_inference_job.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 from telco_churn.common import Job
 from telco_churn.model_inference import ModelInference
@@ -9,23 +10,37 @@ _logger = get_logger()
 
 class ModelInferenceJob(Job):
 
-    def launch(self):
-        _logger.info('Launching Batch ModelInferenceJob job')
-        _logger.info(f'Running model-inference-batch in {os.getenv("DEPLOYMENT_ENV")} environment')
-
+    def _get_model_uri(self) -> str:
         model_registry_name = os.getenv('model_registry_name')
         model_registry_stage = self.conf['mlflow_params']['model_registry_stage']
         model_uri = f'models:/{model_registry_name}/{model_registry_stage}'
 
+        return model_uri
+
+    def _get_inference_data(self) -> str:
+        """
+        Get the name of the table to perform inference on
+        """
+        return self.conf['data_input']['table_name']
+
+    def _get_predictions_output_params(self) -> Dict:
+        """
+        Get a dictionary of delta_path, table_name, mode key-values to pass to run_and_write_batch of ModelInference
+        """
         predictions_table_database_name = os.getenv('predictions_table_database_name')
         predictions_table_name = os.getenv('predictions_table_name')
-        predictions_table_name =
+        output_table_name = f'{predictions_table_database_name}.{predictions_table_name}'
 
-        ModelInference(model_uri=model_uri,
-                       inference_data=self.conf['data_input']['table_name'])\
-            .run_and_write_batch(delta_path=self.conf['data_output']['delta_path'],
-                                 table_name=self.conf['data_output']['table_name'],
-                                 mode=self.conf['data_output']['mode'])
+        return {'delta_path': os.getenv('predictions_table_dbfs_path'),
+                'table_name': output_table_name,
+                'mode': self.conf['data_output']['mode']}
+
+    def launch(self):
+        _logger.info('Launching Batch ModelInferenceJob job')
+        _logger.info(f'Running model-inference-batch in {os.getenv("DEPLOYMENT_ENV")} environment')
+        ModelInference(model_uri=self._get_model_uri(),
+                       inference_data=self._get_inference_data())\
+            .run_and_write_batch(**self._get_predictions_output_params())
         _logger.info('Batch ModelInferenceJob job finished')
 
 
