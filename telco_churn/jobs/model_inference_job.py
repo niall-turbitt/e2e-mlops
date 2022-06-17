fadeca41b5f4a6d30cb4ac22b63fb1a0ce46cc2e
Niall Turbitt <niall.turbitt@databricks.com>
2022-06-17 14:18:03 +0100

    model inference batch update

--- telco_churn/jobs/model_inference_job.py
+++ telco_churn/jobs/model_inference_job.py
@@ -1,3 +1,5 @@
+import os
+
 from telco_churn.common import Job
 from telco_churn.model_inference import ModelInference
 from telco_churn.utils.logger_utils import get_logger
@@ -9,7 +11,9 @@ class ModelInferenceJob(Job):
 
     def launch(self):
         _logger.info('Launching Batch ModelInferenceJob job')
-        model_registry_name = self.conf['mlflow_params']['model_registry_name']
+        _logger.info(f'Running model-inference-batch in {os.getenv("DEPLOYMENT_ENV")} environment')
+
+        model_registry_name = os.getenv('model_registry_name')
         model_registry_stage = self.conf['mlflow_params']['model_registry_stage']
         model_uri = f'models:/{model_registry_name}/{model_registry_stage}'
 
