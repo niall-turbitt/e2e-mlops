09fe9be3fa96ce64fddec5b9da9922d1967b6409
Niall Turbitt <niall.turbitt@databricks.com>
2022-06-23 17:19:38 +0100

    remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import find_packages, setup
 from telco_churn import __version__
 
-with open('requirements.txt') as f:
-    reqs = f.read()
-
 setup(
     name='telco_churn',
     packages=find_packages(exclude=['tests', 'tests.*']),
     setup_requires=['wheel'],
-    install_requires=reqs,
     version=__version__,
     description='Demo repository implementing an end-to-end MLOps workflow on Databricks. Project derived from dbx '
                 'basic python template',
