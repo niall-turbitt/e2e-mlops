275f352eaf8dfd6c24358ab55380bda5d81ba43b
Niall Turbitt <niall.turbitt@databricks.com>
2022-07-25 08:21:34 +0100

    add filters to logger

--- telco_churn/utils/logger_utils.py
+++ telco_churn/utils/logger_utils.py
@@ -1,7 +1,24 @@
 import logging
 
 
+class NoReceivedCommandFilter(logging.Filter):
+    def filter(self, record):
+        return not record.getMessage().startswith('Received command c')
+
+
+class NoPythonDotEnvFilter(logging.Filter):
+    def filter(self, record):
+        return not record.getMessage().startswith('Python-dotenv')
+
+
 def get_logger():
     logging.getLogger('py4j.java_gateway').setLevel(logging.ERROR)
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO)
-    return logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
+
+    filter_1 = NoReceivedCommandFilter()
+    filter_2 = NoPythonDotEnvFilter()
+    logger.addFilter(filter_1)
+    logger.addFilter(filter_2)
+
+    return logger
